4e3084ac6c4e24611a701db16c189b0b58932c06
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-02-16 12:02:16 +0100

    1.5

---
+++ js/1.5.js
@@ -0,0 +1,8 @@
+// var readlineSync = require('readline-sync');
+
+// let firstNumber = readlineSync.question("What's your first number decimal? ");
+// let secondNumber = readlineSync.question("What's your second number decimal? ");
+firstNumber = 2.7;
+secondNumber = 3.4;
+// console.log(Math.trunc(firstNumber));
+console.log(Math.trunc(firstNumber)*secondNumber);
