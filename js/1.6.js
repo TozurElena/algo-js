17ba38665199511cb8119bf1f9966027b5ec1f81
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-02-16 12:09:25 +0100

    1.6

---
+++ js/1.6.js
@@ -0,0 +1,8 @@
+// var readlineSync = require('readline-sync');
+
+// let firstNumber = readlineSync.question("What's your first number ineger? ");
+// let secondNumber = readlineSync.question("What's your second number integer? ");
+let firstNumber = 9;
+let secondNumber = 2;
+
+console.log("rester= " + firstNumber % secondNumber );
