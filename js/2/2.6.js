0f897196fb59579bccff924d7368cd252cac49cb
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-02-16 14:39:12 +0100

    2.6

---
+++ js/2/2.6.js
@@ -0,0 +1,23 @@
+const readlineSync = requiere('readline-sync');
+
+let day = readlineSync.question("Entee  number  between 1 and 7-  ");
+
+
+switch(day){
+  case 1: console.log('Lundi');
+    break;
+  case 2: console.log('Mardi');
+    break;
+  case 3: console.log('Mercredi');
+    break;
+  case 4: console.log('Jeudi');
+    break;
+  case 5: console.log('Vendredi');
+    break;
+  case 6: console.log('Samedi');
+    break;
+  case 7: console.log('Dimanche')
+    break;
+  default: console.log('Erreur, 1-7');
+    
+}
