4b6707efed85e66cb47ad387a70e351f76799d9a
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-02-16 12:34:45 +0100

    2.1

---
+++ js/1.8.js
@@ -0,0 +1,6 @@
+const readlineSync = requiere("readline-sync");
+
+let firstName = readlineSync.question("What's your first name? ");
+let name = readlineSync.question("What's your second name? ");
+let city = readlineSync.question("What's your city? ");
+//pas terminer!!!!
---
+++ js/2.1.js
@@ -0,0 +1,6 @@
+// const readlineSync = requiere("readline-sync");
+
+// let age = readlineSync.question("What's your age? ");
+const age = 46;
+if (age>=18) console.log("You are an adult")
+  else console.log('You are not yet an adult');
