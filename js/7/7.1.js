84bbc79f5983f572c2e9614f5e203aa01a68000a
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-02-22 12:41:03 +0100

    add 7.2.

--- js/7/7.1.js
+++ js/7/7.1.js
@@ -6,7 +6,7 @@ const random = () => {
 
 console.log('Guess the number  an integer between 1 and 100');
 let numberIdeal = random(); let numberUser;
-console.log(numberIdeal);
+// console.log(numberIdeal);
 do {
   numberUser = readlineSync.question("number = ");
   if (numberUser == numberIdeal) console.log('Well guessed!');
---
+++ js/7/7.2.js
@@ -0,0 +1,17 @@
+const readlineSync = require("readline-sync");
+
+let number = readlineSync.question("number = ");
+
+const fib = (number) => {
+  let a = 1; let b = 1;
+  console.log(`1 \n1`);
+  for (let i = 3; i <= number; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+    console.log(`${b} `);
+  }
+  
+}
+
+fib(number);
